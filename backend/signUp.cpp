f442f6ac7a1c755e90edd1e46e13a33e01265618
damsehgal <sehgaldam121@gmail.com>
2017-04-13 01:18:48 +0530

    insert done

--- backend/signUp.cpp
+++ backend/signUp.cpp
@@ -147,8 +147,8 @@ writeUserData(string ID, string emailID, string name, string dateOfBirth, string
 	out3.close();
 	ofstream out4("../ternarySearchInsert/insert.txt");
 	ofstream out5("../tstInsert.txt");
-	out5 << name << " " << id << " " << linkOfProfilePicture;
-	out4 << name << " " << id << " " << linkOfProfilePicture;
+	out5 << name << " " << ID << " " << linkOfProfilePicture;
+	out4 << name << " " << ID << " " << linkOfProfilePicture;
 	out5.close();
 	out4.close();
 }
